f4f58bff217a7f3c1c7d88b5b864834bd52adc00
amCtrld <amctrld@gmail.com>
2024-11-26 09:58:28 +0300

    Project Layout

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CarPort
-Vehicle inventory management.
+
+Desc
+- The demo showcases a sleek, interactive Car Import Management System
+built with Next.js, simulating real-world functionalities like tracking
+shipments, managing vehicle inventory, and automating compliance
+checks — all powered by localStorage for a seamless, server-free
+experience! 
+
+Tech
+- NextJS
+- Tailwind
+
+Storage
+- LocalStorage
---
+++ carport/src/app/page.tsx
@@ -0,0 +1,23 @@
+carport/
+├── src/
+│   ├── app/
+│   │   ├── layout.js (Root layout)
+│   │   ├── page.js (Login page)
+│   │   ├── signup/
+│   │   │   └── page.js (Signup page)
+│   │   ├── dashboard/
+│   │   │   ├── layout.js (Dashboard layout)
+│   │   │   ├── admin/
+│   │   │   │   └── page.js (Admin dashboard)
+│   │   │   ├── customer/
+│   │   │       └── page.js (Customer dashboard)
+├── public/
+│   └── images/
+│       └── logincarbg.jpg
+├── styles/
+│   └── globals.css
+├── components/
+│   ├── AuthButtons.js
+│   ├── InputField.js
+│   ├── Header.js
+└── package.json
